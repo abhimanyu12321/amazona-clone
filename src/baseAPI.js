0af6ffe6d29f178eae279fd063b21de5ac90a0ec
Goru <abhiabhi9087@gmail.com>
2023-09-07 09:40:42 +0530

    changed backend uri

--- src/baseAPI.js
+++ src/baseAPI.js
@@ -1,10 +1,5 @@
-export const baseURL = "https://shopping-app-backend-i62ihnmz8-abhimanyu12321.vercel.app"
+export const baseURL = "https://shopping-app-backend-six.vercel.app"
 
-
-//CURRENT BACKEND URL OF DEPLOYED VERSION  IS GIVEN BELOW
-
-// IF I MADE SOME CHANGES IN BACKEND THEN COPY THE DEPLOYED URL PASTE THE CHANGED URL IN ABOVE VARIABLE AND THEN DEPLOY THE FRONTEND AGAIN TO REFLECT CHANGES
-
-// vercel backend deploy url https://shopping-app-backend-i62ihnmz8-abhimanyu12321.vercel.app
+// vercel backend deploy url https://shopping-app-backend-six.vercel.app
 
 // local host URL http://localhost:4000
