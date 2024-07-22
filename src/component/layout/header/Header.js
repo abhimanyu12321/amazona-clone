0740e02f60fac196d9a9a7b2f46254a361fc060f
Goru <abhiabhi9087@gmail.com>
2024-07-22 16:56:07 +0530

    header-css-change

--- src/component/layout/header/Header.js
+++ src/component/layout/header/Header.js
@@ -29,15 +29,15 @@ export default function Header() {
 
                 </div>
                 <div className="headermid max-w-[700px] flex-grow-[1]">
-                    <form className="headersearchBox flex justify-center items-center hover:border-[3px] hover:border-solid hover:border-[#ff9900] hover:rounded-[14px]" onSubmit={searchSubmitHandler}>
+                    <form className="headersearchBox flex justify-center items-center " onSubmit={searchSubmitHandler}>
                         <input
                             type="text"
                             placeholder="Search Amazon.in"
                             onChange={(e) => setKeyword(e.target.value)}
-                            className='py-[15px] px-[4px] w-[100%] border rounded-tl-[10px] rounded-tr-none rounded-bl-[10px] rounded-br-none box-border outline-none text-black'
+                            className='py-[15px] px-[4px] w-[100%] border rounded-tl-[10px] rounded-tr-none rounded-bl-[10px] rounded-br-none box-border outline-0 text-black'
                         />
                         <button type="submit"
-                            className='border rounded-tr-[10px] rounded-tl-none rounded-br-[10px] rounded-bl-none bg-[#ff9900] py-[19px] px-[1px] w-[50px] cursor-pointer transition-all hover:bg-[#ff7000] flex justify-center items-center'
+                            className='border rounded-tr-[10px] rounded-tl-none rounded-br-[10px] rounded-bl-none bg-[#ff9900] py-[19px] px-[1px] w-[50px] cursor-pointer transition-all  flex justify-center items-center'
                         >
                             <BiSearch />
                         </ button>
@@ -58,7 +58,7 @@ export default function Header() {
 
             {/* For Mobile view */}
             <div className="flex flex-col md:hidden header w-[100vw] bg-[#0f1111]  justify-center items-center text-white gap-[20px] fixed z-10 ">
-                <div className='headerleft flex justify-around items-center gap-[10px] text-[25px] md:text-[25px] pt-[4px]'>
+                <div className='w-full flex justify-around items-center  text-[20px] md:text-[25px] pt-[6px]'>
                     <Link to="/" className="headercolorclass text-white list-none no-underline">
                         <span className='headerleftlogo hover:border hover:border-solid hover:border-white cursor-pointer'>amazon.in</span>
                     </Link>
@@ -66,25 +66,24 @@ export default function Header() {
                         <div className='headerleftproducts hover:border hover:border-solid hover:border-white cursor-pointer hover:p-[0.2rem]'>Products</div>
                     </Link>
                     <Link to="/cart" className="headercolorclass text-white list-none no-underline">
-                        <MdAddShoppingCart className='headercarticon hover:border hover:border-solid hover:border-white cursor-pointer text-[40px]' />
+                        <MdAddShoppingCart className='headercarticon hover:border hover:border-solid hover:border-white cursor-pointer text-[30px]' />
                     </Link>
 
                     <Link to="/login" className="headercolorclass text-white list-none no-underline">
-                        <MdAccountCircle className='headerusericon hover:border hover:border-solid hover:border-white cursor-pointer text-[40px]' />
+                        <MdAccountCircle className='headerusericon hover:border hover:border-solid hover:border-white cursor-pointer text-[30px]' />
                     </Link>
 
                 </div>
-                <div className="headermid mb-[20px] max-w-[700px] flex-grow-[1]">
-                    <form className="headersearchBox flex justify-center items-center hover:border-[3px] hover:border-solid hover:border-[#ff9900] hover:rounded-[14px]" onSubmit={searchSubmitHandler}>
+                <div className="headermid mb-[20px] w-full flex-grow-[1] px-4">
+                    <form className="headersearchBox flex justify-center items-center outline-0" onSubmit={searchSubmitHandler}>
                         <input
                             type="text"
                             placeholder="Search Amazon.in"
                             onChange={(e) => setKeyword(e.target.value)}
-                            className='py-[15px] px-[4px] w-[100%] border rounded-tl-[10px] rounded-tr-none rounded-bl-[10px] rounded-br-none box-border  text-black'
-                            style={{ outline: "none", }}
+                            className='py-[15px] px-[4px] w-[100%] border rounded-tl-[10px] rounded-tr-none rounded-bl-[10px] rounded-br-none  text-black outline-0'
                         />
                         <button type="submit"
-                            className='border rounded-tr-[10px] rounded-tl-none rounded-br-[10px] rounded-bl-none bg-[#ff9900] py-[19px] px-[1px] w-[50px] cursor-pointer transition-all hover:bg-[#ff7000] flex justify-center items-center'
+                            className='border rounded-tr-[10px] rounded-tl-none rounded-br-[10px] rounded-bl-none bg-[#ff9900] py-[19px] px-[1px] w-[50px] cursor-pointer transition-all  flex justify-center items-center outline-none'
                         >
                             <BiSearch />
                         </ button>
